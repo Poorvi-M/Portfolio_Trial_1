ac2ad0ad2a2bffe432ea2c9ff9346f69801d4170
Poorvi <poorvimanu08@gmail.com>
2025-09-03 23:16:11 +0530

    Add files via upload

---
+++ my_portfolio/index.js
@@ -0,0 +1,8 @@
+const roles = ["Web Developer", "Violinist", "Curious Learner"];
+let current = 0;
+
+function toggleRole() {
+  current = (current + 1) % roles.length;
+  document.getElementById("toggle-role").textContent = roles[current];
+}
+setInterval(toggleRole, 2000);
