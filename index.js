ac3c3bfa6dd41b5119d02a6f822b6cb0f5aabb00
Poorvi <poorvimanu08@gmail.com>
2025-09-21 23:22:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,11 +7,9 @@ function toggleRole() {
 }
 setInterval(toggleRole, 2000);
 
-
 const menuBtn = document.getElementById("menu-btn");
 const menu = document.getElementById("menu");
 
 menuBtn.addEventListener("click", () => {
   menu.classList.toggle("hidden");
 });
-
